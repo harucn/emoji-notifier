9dd6e582af70210f9e6b51d18118e61fd15c3c67
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-03-25 00:57:33 +0900

    emoji追加時にログ出力するようにした

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,30 @@
-const { App } = require('@slack/bolt');
+import { App } from '@slack/bolt';
+import { EventFromType } from '@slack/bolt/dist/types/events';
 
-// ボットトークンと Signing Secret を使ってアプリを初期化します
+
+// ボットトークンと Signing Secret を使ってアプリを初期化
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+app.event('emoji_changed', async ({ event, client }) => {
+  try {
+    if(event.subtype === 'add') {
+      const addedEmoji : string = event.name ?? ''
+      console.log(`emoji added : ${addedEmoji}`)
+    }
+
+    console.log('Run Success')
+  }
+  catch (error) {
+    console.error(error)
+  }
+});
+
+
 (async () => {
-  // アプリを起動します
+  // アプリ起動
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
