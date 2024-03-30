4958ee36327d9776befe1145c4cc4d061f8a42df
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-03-30 18:51:05 +0900

    絵文字削除、変更時も通知するようにした

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import { App } from '@slack/bolt';
+import {EmojiChangedEvent} from '@slack/bolt/dist/types/events';
 
 
 // ボットトークンと Signing Secret を使ってアプリを初期化
@@ -9,21 +10,33 @@ const app = new App({
 
 app.event('emoji_changed', async ({ event, client }) => {
   try {
-    if(event.subtype === 'add') {
-      const addedEmoji : string = event.name ?? ''
-      await client.chat.postMessage({
-        channel: process.env.SLACK_CHANNEL_ID ?? '',
-        text: `emoji added : ${addedEmoji}`
+    await client.chat.postMessage({
+        channel: process.env.SLACK_CHANNEL_ID || '',
+        text: createText(event)
       })
-    }
-
-    console.log('Run Success')
-
   } catch (error) {
     console.error(error)
   }
 });
 
+const createText = (event: EmojiChangedEvent): string => {
+  if (event.subtype === 'add') {
+    const addedEmoji: string = event.name ?? ''
+    return `\`${addedEmoji}\` が追加されました！\n` +
+           `:${addedEmoji}:`
+
+  } else if (event.subtype === 'remove') {
+    const separator = ' '
+    const removedEmojis: string[] = event.names ?? []
+    const joined: string = removedEmojis.map((removedEmoji) => `\`${removedEmoji}\``).join(separator)
+    return joined + 'が削除されました :sob:'
+
+  } else if (event.subtype === 'rename') {
+    return `名前が変更されました \`${event.old_name}\`→\`${event.new_name}\`\n` +
+           `:${event.new_name}:`
+  }
+  return ''
+}
 
 (async () => {
   // アプリ起動
