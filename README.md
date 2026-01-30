16e38e217f4a0c9506e4cba5ced3807fd2a39fcb
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2026-01-30 22:25:28 +0900

    Oxfmt 導入

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # emoji-notifier
+
 Slackのemoji更新を通知するbot
 
-
 ## How to use
+
 ### Command
+
 ```sh
 # install package
 npm install
@@ -17,6 +19,7 @@ node dist/app.js
 ```
 
 ### Environment Variables
+
 - `SLACK_BOT_TOKEN` : Bot User OAuth Token
 - `SLACK_SIGNING_SECRET` : Signing Secret
 - `SLACK_CHANNEL_ID` : Channel to notifiy
--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,45 @@
-import { App } from '@slack/bolt';
-import {EmojiChangedEvent} from '@slack/bolt/dist/types/events';
-
+import { App } from "@slack/bolt";
+import { EmojiChangedEvent } from "@slack/bolt/dist/types/events";
 
 // ボットトークンと Signing Secret を使ってアプリを初期化
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
-app.event('emoji_changed', async ({ event, client }) => {
+app.event("emoji_changed", async ({ event, client }) => {
   try {
     await client.chat.postMessage({
-        channel: process.env.SLACK_CHANNEL_ID || '',
-        text: createText(event)
-      })
+      channel: process.env.SLACK_CHANNEL_ID || "",
+      text: createText(event),
+    });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 });
 
 const createText = (event: EmojiChangedEvent): string => {
-  if (event.subtype === 'add') {
-    const addedEmoji: string = event.name ?? ''
-    return `\`${addedEmoji}\` が追加されました！\n` +
-           `:${addedEmoji}:`
-
-  } else if (event.subtype === 'remove') {
-    const separator = ' '
-    const removedEmojis: string[] = event.names ?? []
-    const joined: string = removedEmojis.map((removedEmoji) => `\`${removedEmoji}\``).join(separator)
-    return joined + ' が削除されました:sob:'
-
-  } else if (event.subtype === 'rename') {
-    return `名前が変更されました \`${event.old_name}\`→\`${event.new_name}\`\n` +
-           `:${event.new_name}:`
+  if (event.subtype === "add") {
+    const addedEmoji: string = event.name ?? "";
+    return `\`${addedEmoji}\` が追加されました！\n` + `:${addedEmoji}:`;
+  } else if (event.subtype === "remove") {
+    const separator = " ";
+    const removedEmojis: string[] = event.names ?? [];
+    const joined: string = removedEmojis
+      .map((removedEmoji) => `\`${removedEmoji}\``)
+      .join(separator);
+    return joined + " が削除されました:sob:";
+  } else if (event.subtype === "rename") {
+    return (
+      `名前が変更されました \`${event.old_name}\`→\`${event.new_name}\`\n` + `:${event.new_name}:`
+    );
   }
-  return ''
-}
+  return "";
+};
 
 (async () => {
   // アプリ起動
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
